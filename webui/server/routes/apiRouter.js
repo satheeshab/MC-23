e7eb6f933b5cfdbb174d5ed12efe2fb1651fe5e5
satheeshab <satheeshab@users.noreply.github.com>
2023-07-26 12:53:53 +0530

    refactor: server side code

---
+++ webui/server/routes/apiRouter.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+const { getAdmissionDetails } = require('../middleware/admission');
+const { getCourseDetails, createCourse, deleteCourse } = require('../middleware/course');
+ 
+// admission related routes
+router.get('/admissions/:courseId?', getAdmissionDetails);
+
+// course related routes
+router.get('/course/:courseId?', getCourseDetails);
+router.post('/course', createCourse);
+router.delete('/course/:courseId?', deleteCourse);
+
+module.exports = router;
